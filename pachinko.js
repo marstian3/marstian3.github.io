83d22d56b8087a40119beefc7a322f0f6ea3e295
marstian3 <167735881+marstian3@users.noreply.github.com>
2024-07-26 13:49:31 -0400

    Update pachinko.js

--- pachinko.js
+++ pachinko.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
 canvas.width = 600;
-canvas.height = 800;
+canvas.height = 650;
 
 const pegs = [];
 const balls = [];
